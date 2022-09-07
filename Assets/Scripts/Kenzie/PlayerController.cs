72d9cc9d902fc877dfe0a526714ccd4db54a6486
Kenzie MacDougal <kenzie@macdougal.com>
2022-09-07 13:01:58 -0700

    player moves

--- Assets/Scripts/Kenzie/PlayerController.cs
+++ Assets/Scripts/Kenzie/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector2 dir = Vector2.zero;
+    [SerializeField] private float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnMove(InputValue input)
+    {
+        dir = input.Get<Vector2>();
+        rb.velocity = dir * speed;
     }
 }
