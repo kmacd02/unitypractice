c72e734e8a68886526111bed6147350a351f1e62
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2022-09-07 13:06:29 -0700

    stuff

--- Assets/Scripts/Yun/PlayerController.cs
+++ Assets/Scripts/Yun/PlayerController.cs
@@ -1,20 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Yun{
     public class PlayerController : MonoBehaviour
     {
+        private Rigidbody2D rb;
+
+        [SerializeField] private float speed = 5f;
+        private Vector2 dir = Vector2.zero;
+        
         // Start is called before the first frame update
         void Start()
         {
-
+            rb = GetComponent<Rigidbody2D>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            
+        }
 
+        public void OnMove(InputValue input)
+        {
+            dir = input.Get<Vector2>();
+            rb.velocity = dir * speed;
         }
     }
 }
